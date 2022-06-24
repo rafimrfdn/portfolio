1dd5f172f0df92e51269d898a66e9e25fc27c5b6
MKAbuMattar <mohammad.khaled@outlook.com>
2022-06-25 00:12:16 +0300

    MKABUMATTAR: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 import partytown from '@astrojs/partytown'
-import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable the Preact integration to support Preact JSX components.
-  integrations: [partytown(), preact()],
+  // https://astro.build/config/plugins
+  integrations: [partytown()],
 })
